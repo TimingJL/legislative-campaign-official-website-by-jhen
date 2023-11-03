f1b706331220f170db9758ab6ffbbea180b8eb4b
just_taiming <eefozeo@gmail.com>
2023-11-03 14:10:52 +0800

    feat: add modal to actions

--- src/pages/home/actions/Donate.tsx
+++ src/pages/home/actions/Donate.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import styled from 'styled-components';
+
 import Button from 'src/components/Button';
 import ArrowForward from 'src/assets/icons/arrow-forward.svg';
 import DonateSrc from 'src/assets/images/donate.png';
 import { AnchorId } from 'src/config';
+import Modal from 'src/components/Modal';
+
 import { CommonBox } from './styled';
 
 const DonateBox = styled(CommonBox)`
@@ -11,21 +14,35 @@ const DonateBox = styled(CommonBox)`
 `;
 
 const Donate = () => {
+  const [open ,setOpen] = React.useState(false);
   return (
-    <DonateBox id={AnchorId.donate}>
-      <div>
-        <div className="action__title">小額支持喵喵</div>
-        <div className="action__description">您的小筆捐款，是每隻毛孩未來的大大動力！</div>
-      </div>
-      <div className="action__content">
-        <Button
-          endIcon={<img src={ArrowForward} />}
-        >
-          <h6>小額捐款</h6>
-        </Button>
-        <img src={DonateSrc} className="donate__img" />
-      </div>
-    </DonateBox>
+    <>
+      <DonateBox id={AnchorId.donate}>
+        <div>
+          <div className="action__title">小額支持喵喵</div>
+          <div className="action__description">您的小筆捐款，是每隻毛孩未來的大大動力！</div>
+        </div>
+        <div className="action__content">
+          <Button
+            endIcon={<img src={ArrowForward} />}
+            onClick={() => setOpen(true)}
+          >
+            <h6>小額捐款</h6>
+          </Button>
+          <img src={DonateSrc} className="donate__img" />
+        </div>
+      </DonateBox>
+      <Modal
+        open={open}
+        title="小額捐款"
+        onClose={() => setOpen(false)}
+        content={(
+          <div>
+            Donate
+          </div>
+        )}
+      />
+    </>
   );
 };
 
--- src/pages/home/actions/Service.tsx
+++ src/pages/home/actions/Service.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import styled from 'styled-components';
+
 import Button from 'src/components/Button';
 import ArrowForward from 'src/assets/icons/arrow-forward.svg';
 import DonateSrc from 'src/assets/images/donate.png';
 import { AnchorId } from 'src/config';
+import Modal from 'src/components/Modal';
+
 import { CommonBox } from './styled';
 
 const ServiceBox = styled(CommonBox)`
@@ -11,21 +14,35 @@ const ServiceBox = styled(CommonBox)`
 `;
 
 const Service = () => {
+  const [open ,setOpen] = React.useState(false);
   return (
-    <ServiceBox id={AnchorId.service}>
-      <div>
-        <div className="action__title">民眾服務信箱</div>
-        <div className="action__description">親愛的鄉親，每一位市民的意見都是我們社區前進的原動力</div>
-      </div>
-      <div className="action__content">
-        <Button
-          endIcon={<img src={ArrowForward} />}
-        >
-          <h6>填寫表單</h6>
-        </Button>
-        <img src={DonateSrc} style={{ visibility: 'hidden' }} className="donate__img" />
-      </div>
-    </ServiceBox>
+    <>
+      <ServiceBox id={AnchorId.service}>
+        <div>
+          <div className="action__title">民眾服務信箱</div>
+          <div className="action__description">親愛的鄉親，每一位市民的意見都是我們社區前進的原動力</div>
+        </div>
+        <div className="action__content">
+          <Button
+            endIcon={<img src={ArrowForward} />}
+            onClick={() => setOpen(true)}
+          >
+            <h6>填寫表單</h6>
+          </Button>
+          <img src={DonateSrc} style={{ visibility: 'hidden' }} className="donate__img" />
+        </div>
+      </ServiceBox>
+      <Modal
+        open={open}
+        title="民眾服務信箱"
+        onClose={() => setOpen(false)}
+        content={(
+          <div>
+            Service
+          </div>
+        )}
+      />
+    </>
   );
 };
 
